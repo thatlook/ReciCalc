7a8d724f837454836e988f51eda2904bfb620793
LNBertolini <lunbertolini@gmail.com>
2018-10-24 16:40:24 -0500

    Add database stubs for storing data

--- database/db.js
+++ database/db.js
@@ -19,3 +19,16 @@ module.exports.fetchRecipeById = function(recipeId) {
 module.exports.searchIngredientByName = function(searchString) {
   //look for ingredients that might be the target and return them
 };
+
+module.exports.addIngredient = function(ingredient) {
+  //takes an ingredient object and stores it to the ingredients table
+}
+
+module.exports.addRecipeIngredient = function(recipeIngredient) {
+  //takes an ingredient entry on a recipe and adds it to the recipe_ingredient junction table
+}
+
+module.exports.addRecipe = function(recipe) {
+  //takes a recipe object, adds the basic data to the db, then calls addRecipeIngredient on each 
+  //ingredient entry to store the necessary information
+}
