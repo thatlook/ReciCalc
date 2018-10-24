1ee77b8a376e6a4fe80c57e53838239c4bd118e4
LNBertolini <lunbertolini@gmail.com>
2018-10-24 16:29:54 -0500

    Stub database methods and server endpoints

--- database/db.js
+++ database/db.js
@@ -1,2 +1,21 @@
 // Postgress init here
 // Schema in separate file?
+
+const options = require('db_config');
+
+const knex = require('knex')({
+  client: 'pg',
+  connection: options
+});
+
+module.exports.fetchRecipeList = function() {
+  //return a list of short recipe descriptions
+};
+
+module.exports.fetchRecipeById = function(recipeId) {
+  //return the recipe and all relevant accompanying information
+};
+
+module.exports.searchIngredientByName = function(searchString) {
+  //look for ingredients that might be the target and return them
+};
---
+++ db_config.example.js
@@ -0,0 +1,6 @@
+module.exports = {
+  host : '127.0.0.1',
+  user : 'your_database_user',
+  password : 'your_database_password',
+  database : 'database_name'
+};
