b5af62dccd3957b1a7154250c773901dab6ab1eb
Christopher-Shea <ctshea1@gmail.com>
2018-10-25 16:00:25 -0500

    track user input for create recipe title and description

---
+++ client/src/components/CreateDescription.jsx
@@ -0,0 +1,44 @@
+import React, {Component} from 'react';
+
+class CreateDescription extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          description: null,
+          saved: false
+        }
+      this.updateDescription = this.updateDescription.bind(this);
+      this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    updateDescription(event){
+      this.setState({description: event.target.value});
+    }
+
+    toggleInput(){
+        this.setState(prevState => ({saved: !prevState.saved}));
+    }
+
+    handleSubmit(event){
+      event.preventDefault();
+      if (this.state.saved) {
+        this.toggleInput();
+      } else {
+        this.toggleInput();
+        this.props.updateRecipe('description', this.state.description);
+      }
+    }
+
+    render () {
+        return (
+          <form id='create-description' onSubmit={this.handleSubmit}>
+            <label>
+              Recipe description:
+              <textarea placeholder='recipe description here' onChange={this.updateDescription} disabled={this.state.saved}/>
+            </label>
+            <input className = 'button' type='submit' value={this.state.saved ? 'Edit' : 'Save'} />
+          </form>)
+    }
+}
+
+export default CreateDescription;
---
+++ client/src/components/CreateTitle.jsx
@@ -0,0 +1,44 @@
+import React, {Component} from 'react';
+
+class CreateTitle extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          title: null,
+          saved: false
+        }
+      this.updateTitle = this.updateTitle.bind(this);
+      this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    updateTitle(event){
+      this.setState({title: event.target.value});
+    }
+
+    toggleInput(){
+        this.setState(prevState => ({saved: !prevState.saved}));
+    }
+
+    handleSubmit(event){
+      event.preventDefault();
+      if (this.state.saved) {
+        this.toggleInput();
+      } else {
+        this.toggleInput();
+        this.props.updateRecipe('title', this.state.title);
+      }
+    }
+
+    render () {
+        return (
+          <form id='create-title' onSubmit={this.handleSubmit}>
+            <label>
+              Recipe title:
+              <input type='text' placeholder='recipe title here' onChange={this.updateTitle} disabled={this.state.saved}/>
+            </label>
+            <input className = 'button' type='submit' value={this.state.saved ? 'Edit' : 'Save'} />
+          </form>)
+    }
+}
+
+export default CreateTitle;
