b3c5515973793814bf85b27e71444e64fce1fd13
Christopher-Shea <ctshea1@gmail.com>
2018-10-25 16:37:16 -0500

    working on create ingredients list

---
+++ client/src/components/CreateIngredients.jsx
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+class CreateIngredients extends Component {
+    constructor() {
+        super();
+        this.state = {
+
+        }
+    }
+
+    render () {
+        
+    }
+}
+
+export default CreateIngredients;
+
+
--- client/src/components/IngredientList.jsx
+++
@@ -1,5 +0,0 @@
-// has a ingredient item counter
-// renders ingredient items on click
-// onclikc for save/delete button that adjusts create's state
-
-
