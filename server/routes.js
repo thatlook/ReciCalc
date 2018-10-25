1e00e8f31b2b0e86aed77baccb6d5b16cdcd7d18
LNBertolini <lunbertolini@gmail.com>
2018-10-25 10:19:18 -0500

    Refactor server functions into a controller file

--- server/routes.js
+++ server/routes.js
@@ -1,40 +1,15 @@
 const router = require('express').Router();
+const controller = require('./controller.js');
 
 
-router.get('/recipes', function(req, res) {
-  //query datbase for a list of short recipe descriptions and return them
-  res.status(200).send('Under construction! List of recipes to come.');
-})
+router.get('/recipes', controller.recipes.getList)
+router.get('/recipes/:recipeId', controller.recipes.getOne)
+router.post('/recipes', controller.recipes.post)
 
-router.get('/recipes/:recipeId', function(req, res) {
-  //query databse for detailed information on the given recipe and return it
-  res.status(200).send('Under construction! Recipe information to come.');
-})
-
-router.get('/ingredients', function(req, res) {
-  //Query database for ingredients matching a particular search string, and present them
-  res.status(200).send('Under construction! Possible ingredient matches to come.');
-})
-
-router.get('/ingredients/usda', function(req, res) {
-  //Query USDA NDB API for ingredients matching a particular search string, and present them
-  res.status(200).send('Under construction! POssible NDB ingredient matches to come.');
-})
-
-router.get('/ingredients/usda/:ndbno', function(req, res) {
-  //Query USDA API for nutritional data about the provided item number
-  res.status(200).send('Under consruction! Possible nutritional information to come.');
-})
-
-router.post('/recipes', function(req, res) {
-  //Store recipe in database
-  res.status(404).send('Under construction! We are not currently able to store data.');
-})
-
-router.post('/ingredients', function(req, res) {
-  //Store ingredient in database
-  res.status(404).send('Under construction! We are not currently able to store data.');
-})
+router.get('/ingredients', controller.ingredients.getDbByName)
+router.get('/ingredients/usda', controller.ingredients.getUsdaByName)
+router.get('/ingredients/usda/:ndbno', controller.ingredients.getUsdaIngredientInfo)
+router.post('/ingredients', controller.ingredients.post)
 
 
 module.exports = router;
