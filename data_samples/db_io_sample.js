c13832b719172805d56b70871ff4cb1f47b7a1f1
LNBertolini <lunbertolini@gmail.com>
2018-10-26 14:22:30 -0500

    Begin making data parsers

---
+++ data_samples/db_io_sample.js
@@ -0,0 +1,46 @@
+databaseRecipeOutput = [ 
+  [ 
+    { 
+      id: 1,
+      name: 'test recipe A',
+      description: null,
+      top_ingredients: null,
+      instructions: null,
+      created_at: null,
+      updated_at: null 
+    } 
+  ],
+  [ 
+    { quantity: null,
+      position: null,
+      ndbno: 1,
+      name: 'test ingredient A1',
+      stdAmount: '100.00',
+      kcalPer: null,
+      fatPer: null,
+      satFatPer: null,
+      fiberPer: null,
+      cholesterolPer: null,
+      sodiumPer: null,
+      carbsPer: null,
+      sugarPer: null,
+      proteinPer: null 
+    },  
+    { 
+      quantity: null,
+      position: null,
+      ndbno: 3,
+      name: 'test ingredient A2',
+      stdAmount: '100.00',
+      kcalPer: null,
+      fatPer: null,
+      satFatPer: null,
+      fiberPer: null,
+      cholesterolPer: null,
+      sodiumPer: null,
+      carbsPer: null,
+      sugarPer: null,
+      proteinPer: null 
+    } 
+  ] 
+]
