1da72f0630296b8ef429be0b9f1cd7f2b1eb0cf2
LNBertolini <lunbertolini@gmail.com>
2018-10-26 12:32:05 -0500

    Queries to return recipe list and individual recipe

--- database/db.js
+++ database/db.js
@@ -7,19 +7,45 @@ const knex = require('knex')(options);
 
 // EXAMPLE DATABASE ACCESS FUNCTION:
 //
-//module.exports.checkAccess = function(id) {
-//  return knex
-//     .select('message')
-//     .from('sample')
-//     .where({id})
-//};
+module.exports.checkAccess = function(id) {
+  return knex
+     .select()
+     .from('sample')
+    // .where({id})
+};
 
 module.exports.fetchRecipeList = function() {
   //return a list of short recipe descriptions
+  return knex.select().from('recipes');
 };
+
 
 module.exports.fetchRecipeById = function(recipeId) {
   //return the recipe and all relevant accompanying information
+  
+  const dataNeeded = [
+    knex.select('*').from('recipes').where({id: recipeId}),
+    knex.select({
+      quantity: 'recipe_ingredients.quantity',
+      position: 'recipe_ingredients.list_position',
+      ndbno: 'ingredients.ndbno',
+      name: 'ingredients.name',
+      stdAmount: 'ingredients.std_amount',
+      kcalPer: 'ingredients.kcal_per',
+      fatPer: 'ingredients.fat_per',
+      satFatPer: 'ingredients.sat_fat_per',
+      fiberPer: 'ingredients.fiber_per',
+      cholesterolPer: 'ingredients.cholesterol_per',
+      sodiumPer: 'ingredients.sodium_per',
+      carbsPer: 'ingredients.carbs_per',
+      sugarPer: 'ingredients.sugar_per',
+      proteinPer: 'ingredients.protein_per'
+    })
+    .from('recipe_ingredients')
+    .join('ingredients', 'recipe_ingredients.food_no', '=', 'ingredients.ndbno')
+    .where({'recipe_ingredients.recipe_id': recipeId})
+  ]; 
+  return Promise.all(dataNeeded)
 };
 
 module.exports.searchIngredientByName = function(searchString) {
