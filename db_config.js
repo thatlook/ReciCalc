0c1f23c9581d6a9f38c07fb507ce150b112696e0
Mitchell Coe <mitchellcoe09@gmail.com>
2018-10-29 14:29:50 -0500

    knex stuff

--- db_config.js
+++ db_config.js
@@ -2,8 +2,14 @@ module.exports.location = {
   host : 'localhost',
   user : process.env.user,
   password : process.env.password,
-  database : 'test'
+  database : 'recipes'
 };
+ 
+module.exports.production = {
+  client: 'pg',
+  connection: process.env.DATABASE_URL + '?ssl=true',
+}
 
-module.exports.environment = 'development' 
+
+module.exports.environment = 'production'
   //Used in database/db.js to determine with part of knexfile.js to use
--- knexfile.js
+++ knexfile.js
@@ -25,7 +25,7 @@ module.exports = {
 
   production: {
     client: 'pg',
-    connection: configDB,
+    connection: process.env.DATABASE_URL + '?ssl=true',
     pool: {
       min: 2,
       max: 10
