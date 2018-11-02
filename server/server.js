c859b6ac7845beb07228b490f4bc9fc8be5bcf78
Steven Duncan <steven.k.duncan@gmail.com>
2018-11-01 19:57:14 -0500

    just kidding

--- server/server.js
+++ server/server.js
@@ -4,9 +4,9 @@ const bodyParser = require("body-parser");
 const router = require('./routes.js');
 
 // Auth0 Modules
-const session = require('express-session');
-const passport = require('passport');
-const Auth0Strategy = require('passport-auth0');
+// const session = require('express-session');
+// const passport = require('passport');
+// const Auth0Strategy = require('passport-auth0');
 const userInViews = require('../lib/middleware/userInViews');
 
 // const AUTH0_CONFIG = require('../auth0_config').AUTH0_CONFIG;
@@ -34,35 +34,35 @@ app.use(express.static(__dirname + '/../client/dist'));
 // app.use(session(sess));
 
 // Configure Passport to use Auth0
-const strategy = new Auth0Strategy(
-  {
-    domain: process.env.AUTH0_DOMAIN || AUTH0_CONFIG.domain,
-    clientID: process.env.AUTH0_CLIENT_ID || AUTH0_CONFIG.clientID,
-    clientSecret: process.env.AUTH0_CLIENT_SECRET || AUTH0_CONFIG.secret,
-    callbackURL: process.env.AUTH0_CALLBACK_URL || AUTH0_CONFIG.callbackURL
-  },
-  function (accessToken, refreshToken, extraParams, profile, done) {
-    // accessToken is the token to call Auth0 API (not needed in the most cases)
-    // extraParams.id_token has the JSON Web Token
-    // profile has all the information from the user
-    return done(null, profile);
-  }
-);
+// const strategy = new Auth0Strategy(
+//   {
+//     domain: process.env.AUTH0_DOMAIN || AUTH0_CONFIG.domain,
+//     clientID: process.env.AUTH0_CLIENT_ID || AUTH0_CONFIG.clientID,
+//     clientSecret: process.env.AUTH0_CLIENT_SECRET || AUTH0_CONFIG.secret,
+//     callbackURL: process.env.AUTH0_CALLBACK_URL || AUTH0_CONFIG.callbackURL
+//   },
+//   function (accessToken, refreshToken, extraParams, profile, done) {
+//     // accessToken is the token to call Auth0 API (not needed in the most cases)
+//     // extraParams.id_token has the JSON Web Token
+//     // profile has all the information from the user
+//     return done(null, profile);
+//   }
+// );
 
-passport.use(strategy);
+// passport.use(strategy);
 
-app.use(passport.initialize());
-app.use(passport.session());
+// app.use(passport.initialize());
+// app.use(passport.session());
 
-// You can use this section to keep a smaller payload
-passport.serializeUser(function (user, done) {
-  done(null, user);
-});
+// // You can use this section to keep a smaller payload
+// passport.serializeUser(function (user, done) {
+//   done(null, user);
+// });
 
-passport.deserializeUser(function (user, done) {
-  done(null, user);
-});
-// -------------
+// passport.deserializeUser(function (user, done) {
+//   done(null, user);
+// });
+// // -------------
 
 
 // ROUTES
