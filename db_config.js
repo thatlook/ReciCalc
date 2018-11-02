3f6cbb336fe70a0b65c3320745c08f8861130b88
Steven Duncan <steven.k.duncan@gmail.com>
2018-11-02 14:23:39 -0500

    trying new db_config

--- db_config.js
+++ db_config.js
@@ -2,14 +2,16 @@ module.exports.location = {
   host : 'localhost',
   user : process.env.user,
   password : process.env.password,
+  connection: 'postres://user:password@localhost/recipe',
   database : 'recipe'
 };
  
 module.exports.production = {
   client: 'pg',
+  // connection: process.env.DATABASE_URL + '?ssl=true',
   connection: process.env.DATABASE_URL + '?ssl=true',
 }
 
 
-module.exports.environment = 'development'
+module.exports.environment = 'production'
   //Used in database/db.js to determine with part of knexfile.js to use
