cef908077c5a15b6cd79761f48d87eb56c687e76
Rikki Sasda <rikkisasda@gmail.com>
2018-11-03 13:18:42 -0500

    Created Login Component

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Login from './Login.jsx';
 
 const Header = () => {
   return (
@@ -9,6 +10,7 @@ const Header = () => {
       <Link className='nav link' to='/create'>Create New Recipe</Link>
       <Link className='nav link' to='/recipes'>Recipes</Link>
       <Link className='nav link' to='/search-create'>Search by Ingredients</Link>
+      <Login />
     </div>
   </div>)
 }
