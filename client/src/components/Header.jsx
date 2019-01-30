2052c9923185fce44a87fff2e15a3321a38ede9a
Yirey Suh <yrysuh@gmail.com>
2019-01-30 17:40:02 +0900

    add link on nav to home

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,16 +4,29 @@ import Login from './Login.jsx';
 
 const Header = () => {
   return (
-  <div id='header'>
-    <h1 id='header-logo' className='logo'>RECI-CALC</h1>
-    <div id='nav-links'>
-      <span>
-        <Link className='nav link' to='/create'>Create New Recipe</Link>
-        <Link className='nav link' to='/recipes'>Recipes</Link>
-        <Link className='nav link' to='/search-create'>Search by Ingredients</Link>
-        <Login />
-      </span>
+    <div id="header">
+      <div>
+        <Link to="/landing">
+          <h1 id="header-logo" className="logo">
+            RECI-CALC
+          </h1>
+        </Link>
+      </div>
+      <div id="nav-links">
+        <span>
+          <Link className="nav link" to="/create">
+            Create New Recipe
+          </Link>
+          <Link className="nav link" to="/recipes">
+            Recipes
+          </Link>
+          <Link className="nav link" to="/search-create">
+            Search by Ingredients
+          </Link>
+          {/* <Login /> */}
+        </span>
+      </div>
     </div>
-  </div>)
-}
+  );
+};
 export default Header;
--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -2,22 +2,24 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Header from './Header.jsx';
 import Recipes from './recipes/Recipes.jsx';
-import SearchCreate from './search/SearchCreate.jsx'
-import oneCreate from './create/oneCreate.jsx'
+import SearchCreate from './search/SearchCreate.jsx';
+import oneCreate from './create/oneCreate.jsx';
+import Landing from './Landing.jsx';
 
 const Main = () => (
-  <div id='main'>
+  <div id="main">
     {/* Header is outside of the switch so that it is always displayed (except on landing page) */}
     <Header />
     <Switch>
-      <Route path='/create' component={oneCreate} />
+      <Route path="/landing" component={Landing} />
+      <Route path="/create" component={oneCreate} />
       {/* Recipes itself is a switch to either recipe list view or individual recipe view */}
-      <Route path='/recipes' component={Recipes} />
-      
-      <Route path='/search-create' component={SearchCreate} />
+      <Route path="/recipes" component={Recipes} />
+
+      <Route path="/search-create" component={SearchCreate} />
       {/* can add a fallback error component: <Route component={ErrorNotFound} /> */}
     </Switch>
   </div>
-)
+);
 
 export default Main;
